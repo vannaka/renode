5722a03d66b155f5d1386b6655bc1bdd3b207d19
Luke Mammen <luke@mammen.gg>
2024-05-07 14:03:09 -0500

    Windows build script changes.

---
+++ build2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# export PATH=$PATH:"/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/amd64"
+export PATH=$PATH:"/c/Python37"
+export PATH=$PATH:"/c/Users/mammen/Downloads/x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0/mingw64/bin"
+
+build.sh "$@"
